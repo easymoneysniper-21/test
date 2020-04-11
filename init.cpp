7ca245cd30042dfe0d50942555b24a76f359ac05
Administrator <wgh3314@outlook.com>
2020-04-11 23:37:47 +0800

    anc

---
+++ init.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2020/4/11.
+//
+
+#include "init.h"
---
+++ init.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2020/4/11.
+//
+
+#ifndef STUDY_INIT_H
+#define STUDY_INIT_H
+
+#endif //STUDY_INIT_H
